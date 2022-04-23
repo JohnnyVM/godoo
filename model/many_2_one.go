b064c31992212d732fe32241bb5f39dd25ef25fd
Juan Vázquez Moreno <vmjuan90@gmail.com>
2022-04-23 18:19:07 +0200

    fix tipe of model.ProductTemplate.CategId

---
+++ model/many_2_one.go
@@ -0,0 +1,22 @@
+package model
+
+import (
+	"encoding/json"
+	"errors"
+)
+
+// Many2One is dificult to estandarize becouse the second field is custom
+type Many2One int64
+
+func (s *Many2One) UnmarshalJSON(data []byte) error {
+	var slice []any
+	if json.Unmarshal(data, &slice) == nil {
+		if len(slice) == 2 {
+			if data, ok := slice[0].(int64); ok {
+				*s = Many2One(data)
+				return nil
+			}
+		}
+	}
+	return errors.New("Could not unmarshall Many2One")
+}
