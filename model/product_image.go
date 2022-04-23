edc297c425f1d738b2d79a05ed13641aed7096e8
Juan Vázquez Moreno <vmjuan90@gmail.com>
2022-04-24 00:22:59 +0200

    use omit empty

--- model/product_image.go
+++ model/product_image.go
@@ -5,6 +5,6 @@ type ProductImage struct {
 	table     string     `value:"product.image"`
 	Id        int64      `json:"id"`
 	Name      string     `json:"name"`
-	VideoUrl  OdooString `json:"video_url"`
-	Image1920 Binary     `json:"image_1920"`
+	VideoUrl  OdooString `json:"video_url,omitempty"`
+	Image1920 Binary     `json:"image_1920,omitempty"`
 }
--- model/product_template.go
+++ model/product_template.go
@@ -9,13 +9,13 @@ type ProductTemplate struct {
 	PurchaseOk              bool       `json:"purchase_ok"`
 	Name                    string     `json:"name"`
 	CategId                 Many2One   `json:"categ_id,omitempty"`
-	Barcode                 OdooString `json:"barcode"`
-	DefaultCode             OdooString `json:"default_code"`
-	Type                    string     `json:"type"` // posible values consu, service, product
-	TaxesId                 []int64    `json:"taxes_id"`
-	SupplierTaxesId         []int64    `json:"supplier_taxes_id"`
-	PublicCategIds          []int64    `json:"public_categ_ids"`
+	Barcode                 OdooString `json:"barcode,omitempty"`
+	DefaultCode             OdooString `json:"default_code,omitempty"`
+	Type                    string     `json:"type,omitempty"` // posible values consu, service, product
+	TaxesId                 []int64    `json:"taxes_id,omitempty"`
+	SupplierTaxesId         []int64    `json:"supplier_taxes_id,omitempty"`
+	PublicCategIds          []int64    `json:"public_categ_ids,omitempty"`
 	StandardPrice           float64    `json:"standard_price"` // cost
-	Image1920               Binary     `json:"image_1920"`
-	ProductTemplateImageIds []int64    `json:"product_template_image_ids"`
+	Image1920               Binary     `json:"image_1920,omitempty"`
+	ProductTemplateImageIds []int64    `json:"product_template_image_ids,omitempty"`
 }
